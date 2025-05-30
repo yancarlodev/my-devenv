486659faf2a09a19642c45208e62dc1e8245bf4f
Yan Lepri <20251CAS10010011@estudantes.ifpr.edu.br>
2025-05-30 13:51:58 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "My development environment";
+
+  inputs =
+    {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+      java-nixpkgs.url = "github:nixos/nixpkgs/12a55407652e04dcf2309436eb06fef0d3713ef3";
+    };
+
+  outputs = { self, nixpkgs, java-nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      java-pkgs = java-nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system} =
+        let
+          common-tools = with pkgs; [ 
+            git
+            curl
+            direnv
+            lolcat
+          ];
+        in {
+          ifpr = pkgs.mkShell {
+            nativeBuildInputs = common-tools ++ (with pkgs; [
+              java-pkgs.jdk24
+              gnumake
+            ]);
+
+            shellHook = ''
+              echo "In IFPR development environment" | lolcat
+            '';
+          };
+
+          go = pkgs.mkShell {
+            nativeBuildInputs = common-tools ++ (with pkgs; [
+              go
+            ]);
+          };
+        };
+    };
+}
